53b7ccdadc0ab763327a551a4e73c0e0984e3e38
PreslavG <138732628+PreslavG@users.noreply.github.com>
2025-09-01 20:43:45 +0300

    Update socket.js

--- webcall/src/scripts/socket.js
+++ webcall/src/scripts/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://render-host-z9qz.onrender.com", {
   transports: ["websocket"],
+  withCredentials: true, // if your backend uses credentials
 });
 
 socket.on("connect", () => {
