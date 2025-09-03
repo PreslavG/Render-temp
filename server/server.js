7e4d0d27c2a33be6b79248f5e7aa1b4ea653aa24
PreslavG <138732628+PreslavG@users.noreply.github.com>
2025-09-03 17:18:47 +0300

    links updated

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://frontend-deployment-nn8o.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -57,7 +57,7 @@ io.on("connection", (socket) => {
 });
 
 // Render sets process.env.PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Socket.IO server running on port ${PORT}`);
 });
